dff0abb81909158d90d632d1a6531efe97e60f16
Nathan Moore <n@nmoore.net>
2018-11-02 03:58:44 -0600

    syntax error

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,4 +5,5 @@ class ContactMailer < ApplicationMailer
     @user = params[:user]
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
 end
