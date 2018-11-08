d07c9f9c9d00562b4bcf8e2ecef36fe43367ac8c
Nathan Moore <n@nmoore.net>
2018-11-08 02:04:53 -0700

    contact modal updates

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,6 +4,7 @@ class ContactMailer < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_contact_email(contact)
     @contact = contact
+    @to Rails.env.development? ? 'n@nmoore.net' : 'nate@nmoore.net'
     mail( :from => @contact.email, :to => 'n@nmoore.net',
     :subject => 'New contact message from ideason.com' )
   end
