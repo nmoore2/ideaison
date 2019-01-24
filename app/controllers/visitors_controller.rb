b5d6bb9b5eb563ff566df971c79907ebed0256da
Nathan Moore <n@nmoore.net>
2019-01-24 16:33:32 -0700

    merge

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -22,6 +22,32 @@ class VisitorsController < ApplicationController
 
   def blog_detail
 
+=======
+    linkedin_api_key = "78t99ayw8dgyz1"
+    secret_key = "2NgT4jBSYOI4F1kT"
+
+
+#     @code = params[:code]
+#     params = {'grant_type' => 'authorization_code', 'code' => @code, 'redirect_uri' => 'http://localhost:3000/blog', 'client_id' => '78t99ayw8dgyz1', 'client_secret' => '2NgT4jBSYOI4F1kT'
+# }
+#      if @code.present?
+#       # post to LinkedIn to get access_token
+#       x = Net::HTTP.post_form(URI.parse('https://www.linkedin.com/oauth/v2/accessToken'), params)
+#       puts x.body
+#       data = JSON.parse(x.body)
+#       @access_token = data["access_token"]
+#       puts 'Access token= ' + @access_token
+#
+#       url = 'https://api.linkedin.com/v2/originalArticles/battling-imposter-syndrom-lauren-kashuk'
+#       headers = {
+#         authorization: 'Bearer ' + @access_token
+#       }
+#
+#       response = HTTParty.get(url, headers: headers)
+#       puts response.body
+
+    end
+>>>>>>> Stashed changes
   end
 
   def services
